a6b8f449301a1d03ddbfe2e93109d18592428445
XxleyderBetaxX <leydermanuelbetancourt@gmail.com>
2025-10-20 11:36:48 -1100

    Se añadieron imagenes y una primera vista del vegetableGarden

--- components/main.js
+++ components/main.js
@@ -1,5 +1,6 @@
 import { HomeView } from '../views/homeView.js';
 import { LoginView } from '../views/loginView.js';
+import { VegetableView } from '../views/vegetableGardenView.js';
 
 
 const app = Vue.createApp({
@@ -7,6 +8,7 @@ const app = Vue.createApp({
     data() {
         return {
             currentView: 'home-view'
+            
             
 
         }
@@ -40,6 +42,7 @@ const app = Vue.createApp({
 
 app.component('home-view', HomeView);
 app.component('login-view', LoginView);
+app.component('garden-view', gardenView);
 
 
 // Montar la app
