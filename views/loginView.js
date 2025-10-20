df0339f73ae31eb73b834b51bd41b8dd2246f012
FranB84 <cinybo4@gmail.com>
2025-10-20 10:48:47 -0600

    Agregar componente homeView con su diseño listo y botón de iniciar sesión funcionando

---
+++ views/loginView.js
@@ -0,0 +1,29 @@
+export const LoginView =  {
+
+    props: {
+
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+
+
+    template: /*html*/`
+    <div>
+        <h1>Login</h1>
+        <p>Aquí va el formulario de inicio de sesión</p>
+    </div>
+    
+
+    `
+
+
+    
+
+};
+
